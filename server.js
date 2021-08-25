1d58ecb7882c74d6620c4ec492200216b3e3f454
Glitch (abstracted-translucent-orbit) <none>
2021-08-25 11:46:18 +0000

    👶👠 Checkpoint ./server.js:24332185/8

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ client.on("messageCreate", async message => {
     name: client.user.username,
     owner: "XR7",
     user: message.author.id,
-    language:"en"
+    language:"th"
   }).then(reply => {
     message.channel.sendTyping();
     message.reply(`${reply}`);
